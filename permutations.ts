024fd355c5e7f65d4e2906459d15b06a40f3a4df
Saúl Ramos <saul.ramos@encora.com>
2024-07-29 09:32:08 -0700

    Add permutations solution

---
+++ permutations.ts
@@ -0,0 +1,21 @@
+function permute(nums: number[]): number[][] {
+  if (nums.length === 1) {
+    return [nums.slice()];
+  }
+
+  const result: Array<Array<number>> = [];
+
+  for (let i = 0; i < nums.length; i++) {
+    const firstNumber = nums.shift()!;
+    const permutations = permute(nums.slice());
+
+    for (const permutation of permutations) {
+      permutation.push(firstNumber);
+    }
+
+    result.push(...permutations);
+    nums.push(firstNumber);
+  }
+
+  return result;
+}
